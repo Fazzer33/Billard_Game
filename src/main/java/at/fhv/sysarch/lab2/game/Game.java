5d7e129aaa43657a4665c5761fff4dc030c8775f
ffa6995 <59058423+ffa6995@users.noreply.github.com>
2020-11-11 15:49:39 +0100

    initial commit

---
+++ src/main/java/at/fhv/sysarch/lab2/game/Game.java
@@ -0,0 +1,108 @@
+package at.fhv.sysarch.lab2.game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import at.fhv.sysarch.lab2.physics.BallPocketedListener;
+import at.fhv.sysarch.lab2.physics.BallsCollisionListener;
+import at.fhv.sysarch.lab2.physics.ObjectsRestListener;
+import at.fhv.sysarch.lab2.rendering.Renderer;
+import javafx.scene.input.MouseEvent;
+
+public class Game implements BallPocketedListener, BallsCollisionListener, ObjectsRestListener {
+    private final Renderer renderer;
+
+    public Game(Renderer renderer) {      
+        this.renderer = renderer;
+        this.initWorld();
+    }
+
+    public void onMousePressed(MouseEvent e) {
+        double x = e.getX();
+        double y = e.getY();
+
+        double pX = this.renderer.screenToPhysicsX(x);
+        double pY = this.renderer.screenToPhysicsY(y);
+    }
+
+    public void onMouseReleased(MouseEvent e) {
+    }
+
+    public void setOnMouseDragged(MouseEvent e) {
+        double x = e.getX();
+        double y = e.getY();
+
+        double pX = renderer.screenToPhysicsX(x);
+        double pY = renderer.screenToPhysicsY(y);
+    }
+
+    private void placeBalls(List<Ball> balls) {
+        Collections.shuffle(balls);
+
+        // positioning the billard balls IN WORLD COORDINATES: meters
+        int row = 0;
+        int col = 0;
+        int colSize = 5;
+
+        double y0 = -2*Ball.Constants.RADIUS*2;
+        double x0 = -Table.Constants.WIDTH * 0.25 - Ball.Constants.RADIUS;
+
+        for (Ball b : balls) {
+            double y = y0 + (2 * Ball.Constants.RADIUS * row) + (col * Ball.Constants.RADIUS);
+            double x = x0 + (2 * Ball.Constants.RADIUS * col);
+
+            b.setPosition(x, y);
+            b.getBody().setLinearVelocity(0, 0);
+            renderer.addBall(b);
+
+            row++;
+
+            if (row == colSize) {
+                row = 0;
+                col++;
+                colSize--;
+            }
+        }
+    }
+
+    private void initWorld() {
+        List<Ball> balls = new ArrayList<>();
+        
+        for (Ball b : Ball.values()) {
+            if (b == Ball.WHITE)
+                continue;
+
+            balls.add(b);
+        }
+       
+        this.placeBalls(balls);
+
+        Ball.WHITE.setPosition(Table.Constants.WIDTH * 0.25, 0);
+        
+        renderer.addBall(Ball.WHITE);
+        
+        Table table = new Table();
+        renderer.setTable(table);
+    }
+
+    @Override
+    public boolean onBallPocketed(Ball b) {
+        return false;
+    }
+
+    @Override
+    public void onBallsCollide(Ball b1, Ball b2) {
+
+    }
+
+    @Override
+    public void onEndAllObjectsRest() {
+
+    }
+
+    @Override
+    public void onStartAllObjectsRest() {
+
+    }
+}
---
+++ src/main/java/at/fhv/sysarch/lab2/physics/PhysicsEngine.java
@@ -0,0 +1,95 @@
+package at.fhv.sysarch.lab2.physics;
+
+import at.fhv.sysarch.lab2.game.Game;
+import at.fhv.sysarch.lab2.rendering.FrameListener;
+import org.dyn4j.dynamics.Step;
+import org.dyn4j.dynamics.StepListener;
+import org.dyn4j.dynamics.World;
+import org.dyn4j.dynamics.contact.ContactListener;
+import org.dyn4j.dynamics.contact.ContactPoint;
+import org.dyn4j.dynamics.contact.PersistedContactPoint;
+import org.dyn4j.dynamics.contact.SolvedContactPoint;
+
+public class PhysicsEngine implements StepListener, ContactListener, FrameListener {
+    private BallPocketedListener ballPocketedListener;
+    private BallsCollisionListener ballsCollisionListener;
+    private ObjectsRestListener objectsRestListener;
+
+    public PhysicsEngine() {
+        World world = new World();
+        world.setGravity(World.ZERO_GRAVITY);
+        world.addListener(this);
+    }
+
+    // Step Listener wird bei jedem Frame aufgerufen
+    // Contact wird bei collission aufgerufen
+    @Override
+    public void begin(Step step, World world) {
+
+    }
+
+    @Override
+    public void updatePerformed(Step step, World world) {
+
+    }
+
+    @Override
+    public void postSolve(Step step, World world) {
+
+    }
+
+    @Override
+    public void end(Step step, World world) {
+
+    }
+
+    // Frame Listener
+    @Override
+    public void onFrame(double dt) {
+
+    }
+
+    // Contact Listener
+    @Override
+    public void sensed(ContactPoint point) {
+
+    }
+
+    @Override
+    public boolean begin(ContactPoint point) {
+
+        return false;
+    }
+
+    @Override
+    public void end(ContactPoint point) {
+
+    }
+
+    @Override
+    public boolean persist(PersistedContactPoint point) {
+        return false;
+    }
+
+    @Override
+    public boolean preSolve(ContactPoint point) {
+        return false;
+    }
+
+    @Override
+    public void postSolve(SolvedContactPoint point) {
+
+    }
+
+    public void addBallPocketedListener(BallPocketedListener listener) {
+        ballPocketedListener = listener;
+    }
+
+    public void addBallsCollisionListener(BallsCollisionListener listener) {
+        ballsCollisionListener = listener;
+    }
+
+    public void addObjectsRestListener(ObjectsRestListener listener) {
+        objectsRestListener = listener;
+    }
+}
